f0ec70b0815488a16dcc1cd60a4c97f04ba73596
golivaresm <93689487+golivaresm@users.noreply.github.com>
2022-03-03 21:42:10 -0300

    Update page_task.py

--- src/page_task.py
+++ src/page_task.py
@@ -3,15 +3,7 @@ import streamlit as st
 def task():
     
     text =  """
-            # Tengo dudas, pero paso de leerme toda la intranet
-            ## Vamos a intentar resolverlas de forma automatica.
-
-            Para ello:
-
-            1. Vamos a ver si hay un modelo de Machine Learning que nos haga el apaño.
-            2. Vamos a proporcionar el texto sobre el cual queremos hacer las preguntas. Este texto se llamará con el nombre técnico "contexto".
-            3. Vamos a proporcionar una serie de preguntas.
-            4. Vamos a ver qué pasa. Si funciona o no, y cuales son los resultados.
+           Bienvenido al QArch. Hoy veremos MASP de Lina Bo Bardi
             """
 
     st.write(text)
